524f78b948b21068a104e62efb4bba0aa5134ab8
PriyankaBawanna <PriyankaBawanna@CESIT-LAP-0124>
2022-05-03 22:20:33 +0530

    Login API for Parent and Teacher

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const studentSchema = new mongoose.Schema({
   studentName: String,
   studentEmail: String,
   studentStandard: String,
-  studentRollNo: Number,
+  studentRollNo: String,
 });
 const teacherSchema = new mongoose.Schema({
   teacherNo: String,
@@ -39,26 +39,65 @@ const teacherSchema = new mongoose.Schema({
 });
 const parentSchema = new mongoose.Schema({
   parentName: String,
-  studentRollNo: Number,
+  studentRollNo: String,
   mobilenumber: Number,
   parentEmail: String,
 });
 
-const studentMarksSchema = new mongoose.Schema({
+const studentMarksSheetSchema = new mongoose.Schema({
   englishMarks: Number,
   hindiMarks: Number,
   science: Number,
   socialScience: Number,
   mathMarks: Number,
-  TotalMarks: Number,
+  Total: Number,
+  studentName: String,
+  studentEmail: String,
+  studentStandard: String,
+  studentRollNo: String,
+});
+
+const studentMarksSheetSchemaTermTwo = new mongoose.Schema({
+  englishMarksTermTwo: Number,
+  hindiMarksTermTwo: Number,
+  scienceTermTwo: Number,
+  socialScienceTermTwo: Number,
+  mathMarksTermTwo: Number,
+  TotalTermTwo: Number,
+  studentName: String,
+  studentEmail: String,
+  studentStandard: String,
+  studentRollNo: String,
+});
+const studentMarksSheetSchemaTermThree = new mongoose.Schema({
+  englishMarksTermThree: Number,
+  hindiMarksTermThree: Number,
+  scienceTermThree: Number,
+  socialScienceTermThree: Number,
+  mathMarksTermThree: Number,
+  TotalTermThree: Number,
+  studentName: String,
+  studentEmail: String,
+  studentStandard: String,
+  studentRollNo: String,
 });
 
 const User = new mongoose.model("user", userSchema);
 const Studentinfo = new mongoose.model("student", studentSchema);
 const Teacherinfo = new mongoose.model("teacher", teacherSchema);
 const ParentInfo = new mongoose.model("parent", parentSchema);
-const studentMarks = new mongoose.model("studentMarks", studentMarksSchema);
-
+const studentMarksSheet = new mongoose.model(
+  "studentMarks",
+  studentMarksSheetSchema
+);
+const studentMarksTermTwo = new mongoose.model(
+  "studentMarksTermTwo ",
+  studentMarksSheetSchemaTermTwo
+);
+const studentMarksTermThree = new mongoose.model(
+  "studentMarksTermThree ",
+  studentMarksSheetSchemaTermThree
+);
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
@@ -115,7 +154,7 @@ app.post("/register", function (req, res) {
 app.post("/addStudent", function (req, res) {
   const { studentName, studentEmail, studentStandard, studentRollNo } =
     req.body;
-  Studentinfo.findOne({ studentEmail: studentEmail }, (err, student) => {
+  Studentinfo.findOne({ studentRollNo: studentRollNo }, (err, student) => {
     if (student) {
       console.log("message: user already register");
       res.send({ message: "user already register" });
@@ -141,29 +180,33 @@ app.post("/addStudent", function (req, res) {
 
 app.post("/addTeacher", function (req, res) {
   const { teacherNo, teacherName, teacherMobileNo, teacherEmailId } = req.body;
-  Teacherinfo.findOne({ teacherEmailId: teacherEmailId }, (err, teacher) => {
-    if (teacher) {
-      console.log("teacher is already present ");
-      res.send({ message: "teacher is already present" });
-    } else {
-      const teacher = new Teacherinfo({
-        teacherNo,
-        teacherName,
-        teacherMobileNo,
-        teacherEmailId,
-      });
-      teacher.save((err) => {
-        if (err) {
-          res.send(err);
-        } else {
-          res.send({ message: "sucessfully ADD" });
-        }
-      });
+  Teacherinfo.findOne(
+    { teacherEmailId: teacherEmailId },
+
+    (err, teacher) => {
+      if (teacher) {
+        console.log("teacher is already present ");
+        res.send({ message: "teacher is already present" });
+      } else {
+        const teacher = new Teacherinfo({
+          teacherNo,
+          teacherName,
+          teacherMobileNo,
+          teacherEmailId,
+        });
+        teacher.save((err) => {
+          if (err) {
+            res.send(err);
+          } else {
+            res.send({ message: "sucessfully ADD" });
+          }
+        });
+      }
     }
-  });
+  );
 });
 
-//add Parent
+//add Parent--***
 app.post("/addParent", function (req, res) {
   const { parentName, studentRollNo, mobilenumber, parentEmail } = req.body;
   ParentInfo.findOne({ parentEmail: parentEmail }, (err, parent) => {
@@ -302,7 +345,7 @@ app.get("/teacherInfo/:id", async (req, res) => {
 
 //getting user profile (dynamic)
 app.get("/profile/:id", async (req, res) => {
-  const UserProfileData = await User.findOne({ _id: req.params.id });
+  const UserProfileData = await User.findOne({ email: req.params.email });
   if (UserProfileData) {
     res.send(UserProfileData);
   } else {
@@ -310,40 +353,31 @@ app.get("/profile/:id", async (req, res) => {
   }
 });
 
-//Search  Student Data
-// app.get("/SearchStudentData/:id", async (req, res) => {
-//   let StudentSearch = await Studentinfo.find({
-//     $or: [
-//       {
-//         studentName: { $regex: req.params.key },
-//       },
-//       {
-//         studentEmail: { $regex: req.params.key },
-//       },
-//     ],
-//   });
-//   res.send(StudentSearch);
-// });
-
-//Add Student Marks Details
-
 app.post("/addStudentMarksStudent", function (req, res) {
   const {
     hindiMarks,
     englishMarks,
     science,
-    socialScienceMarks,
+    socialScience,
     mathMarks,
-    TotalMarks,
+    Total,
+    studentName,
+    studentEmail,
+    studentStandard,
+    studentRollNo,
   } = req.body;
 
-  const marks = new studentMarks({
+  const marks = new studentMarksSheet({
     hindiMarks,
     englishMarks,
     science,
-    socialScienceMarks,
+    socialScience,
     mathMarks,
-    TotalMarks,
+    Total,
+    studentName,
+    studentEmail,
+    studentStandard,
+    studentRollNo,
   });
   marks.save((err) => {
     if (err) {
@@ -355,6 +389,77 @@ app.post("/addStudentMarksStudent", function (req, res) {
   });
 });
 
+//add student MarksDetails for term two
+app.post("/addStudentMarksStudentTermTwo", function (req, res) {
+  const {
+    englishMarksTermTwo,
+    hindiMarksTermTwo,
+    scienceTermTwo,
+    socialScienceTermTwo,
+    mathMarksTermTwo,
+    TotalTermTwo,
+    studentName,
+    studentEmail,
+    studentStandard,
+    studentRollNo,
+  } = req.body;
+
+  const marksTermTwo = new studentMarksTermTwo({
+    englishMarksTermTwo,
+    hindiMarksTermTwo,
+    scienceTermTwo,
+    socialScienceTermTwo,
+    mathMarksTermTwo,
+    TotalTermTwo,
+    studentName,
+    studentEmail,
+    studentStandard,
+    studentRollNo,
+  });
+  marksTermTwo.save((err) => {
+    if (err) {
+      console.log("Student Marks err");
+      res.send({ message: "error" });
+    } else {
+      res.send({ message: "Marks add successfully" });
+    }
+  });
+});
+app.post("/addStudentMarksStudentTermThree", function (req, res) {
+  const {
+    englishMarksTermThree,
+    hindiMarksTermThree,
+    scienceTermThree,
+    socialScienceTermThree,
+    mathMarksTermThree,
+    TotalTermThree,
+    studentName,
+    studentEmail,
+    studentStandard,
+    studentRollNo,
+  } = req.body;
+
+  const marksTermThree = new studentMarksTermThree({
+    englishMarksTermThree,
+    hindiMarksTermThree,
+    scienceTermThree,
+    socialScienceTermThree,
+    mathMarksTermThree,
+    TotalTermThree,
+    studentName,
+    studentEmail,
+    studentStandard,
+    studentRollNo,
+  });
+  marksTermThree.save((err) => {
+    if (err) {
+      console.log("Student Marks err");
+      res.send({ message: "error" });
+    } else {
+      res.send({ message: "Marks add successfully" });
+    }
+  });
+});
 /* Search API for the Student  */
 app.get("/StudentSearch/:key", async (req, resp) => {
   let StudentSearch = await Studentinfo.find({
@@ -364,7 +469,72 @@ app.get("/StudentSearch/:key", async (req, resp) => {
   resp.send(StudentSearch);
 });
 
+//get API for fetch Student Term One Data
+app.get("/TermOneStudentMarksDetails", async (req, res) => {
+  studentMarksSheet.find({}, (err, data) => {
+    console.log("Student Marks details term two ", data);
+    res.send(data);
+  });
+});
+
+//get API for fetch Student Term Two Data
+app.get("/TermTwoStudentMarksDetails", async (req, res) => {
+  studentMarksTermTwo.find({}, (err, data) => {
+    console.log("Student Marks details term two ", data);
+    res.send(data);
+  });
+});
+
+//get API for fetch Student Term Three Data
+app.get("/TermThreeStudentMarksDetails/", async (req, res) => {
+  studentMarksTermThree.find({}, (err, data) => {
+    console.log("Student Marks details term three ", data);
+    res.send(data);
+  });
+});
+
+/*get Term3 the Student Data  according student Roll No */
+app.get("/TermThree/:key", async (req, res) => {
+  if (req.params.key) {
+    let TermThree = await studentMarksTermThree.findOne({
+      $or: [{ studentRollNo: { $regex: req.params.key } }],
+    });
+    res.send(TermThree);
+  } else {
+    res.send({ message: "No Data found" });
+  }
+});
+
+app.post("/teacherLogin", async (req, res) => {
+  console.log(req.body);
+  if (req.body.teacherEmailId && req.body.teacherNo) {
+    let TeacherLogin = await Teacherinfo.findOne(req.body);
+    if (TeacherLogin) {
+      res.send(TeacherLogin);
+    } else {
+      res.send({ message: "No User Found " });
+    }
+  } else {
+    res.send({ message: "No User Found " });
+  }
+});
+
+app.post("/parentLogin", async (req, res) => {
+  if (req.body.studentRollNo && req.body.parentEmail) {
+    let ParentLogin = await ParentInfo.findOne(req.body);
+    if (ParentLogin) {
+      res.send(ParentLogin);
+    } else {
+      res.send({ message: "No User Found " });
+    }
+  } else {
+    res.send({ message: "No User Found " });
+  }
+});
+
+/*Search API for the Teacher  search */
 app.get("/TeacherSearch/:key", async (req, res) => {
+  console.log("what kind of data is pass", req.params.key);
   let TeacherSearch = await Teacherinfo.find({
     $or: [{ teacherName: { $regex: req.params.key } }],
   });
